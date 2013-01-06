40764d8d2e1dfd427b5d4180c879aa70430078da
meoooh <hgk617@naver.com>
2013-01-05 23:17:40 -0500

    의미없는 commit

---
+++ README.md
@@ -0,0 +1,13 @@
+This project is making costumer relationship management system.
+
+[root@localhost ~]# cat /etc/issue
+Fedora release 17 (Beefy Miracle)
+	Kernel \r on an \m (\l)
+[root@localhost ~]# uname -a
+Linux localhost.localdomain 3.6.10-2.fc17.i686 #1 SMP Tue Dec 11 18:33:15 UTC 2012 i686 i686 i386 GNU/Linux
+[root@localhost ~]# django-admin.py --version
+1.4.3
+[root@localhost ~]# python --version
+Python 2.7.3
+[root@localhost ~]# mysql --version
+mysql  Ver 14.14 Distrib 5.5.28, for Linux (i686) using readline 5.1
--- crm/views.py
+++ crm/views.py
@@ -2,13 +2,13 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
-@login_required
+@login_required #from django.contrib.auth.decorators import login_required
 def mainPage(request):
-	variables = RequestContext(request, {
+	variables = RequestContext(request, { # from django.template import RequestContext
 			'username': request.user.username,
 			})
 	
-	return render_to_response('mainPage.html', variables)
+	return render_to_response('mainPage.html', variables) # from django.shortcuts import render_to_response
 
 def userRegister(request):
 	pass
